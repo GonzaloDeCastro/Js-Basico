cc94abd2318153f96c625a126704d983ea136e16
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-14 04:16:10 -0300

    2 - Strings Ejercicio - b

---
+++ 2b.js
@@ -0,0 +1,14 @@
+/*
+Strings
+b) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los
+primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
+*/
+
+var palabra1, palabra2;
+
+palabra1 = "Utilizando oración de ejemplo";
+
+palabra2 = palabra1.substring(0, 4);
+
+console.log(palabra2);
+
