8b0c19fefa855716d3cb6b97201dd95702c9c7de
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-14 04:06:51 -0300

    2 - Strings Ejercicio - a

---
+++ 2a.js
@@ -0,0 +1,11 @@
+/*
+Strings
+a) Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en
+mayúscula (utilizar toUpperCase).
+*/
+
+var palabra;
+
+palabra = 'Esta es una oración de prueba';
+
+console.log(palabra.toUpperCase());
