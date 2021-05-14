8e03ece4336a9da2d67f5962f79067b0f88f03d3
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-14 03:21:44 -0300

    1 - Variables y Operadores Ejercicio - a

---
+++ 1a.js
@@ -0,0 +1,12 @@
+/* Variables y Operadores
+    a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de
+    ambos números en una 3er variable.*/
+
+
+var a, b, suma;
+a = 6;
+b = 4;
+
+suma = a + b;
+
+console.log(suma);
