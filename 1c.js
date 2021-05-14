c762daa87324aea53aecd5e3792120c75e9289a7
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-14 04:01:19 -0300

    1 - Variables y Operadores Ejercicios - c

---
+++ 1c.js
@@ -0,0 +1,14 @@
+/*
+Variables y Operadores
+c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+guardando el resultado la suma una 3er variable (utilizar length).
+*/
+
+var string1, string2, sumaletras;
+
+string1 = "hola";
+string2 = "mundo";
+
+sumaletras = string1.length + string2.length;
+
+console.log(sumaletras);
