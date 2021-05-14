9c08bc53a0b94914baa1c3a4e543c87723c8fb5b
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-14 03:45:03 -0300

    1 - Variables y Operadres Ejercicio - b

---
+++ 1b.js
@@ -0,0 +1,14 @@
+/*
+Variables y Operadores
+b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+*/
+
+var string1, string2, concatenacion;
+
+string1 = "hola ";
+string2 = "mundo";
+
+concatenacion = string1+string2;
+
+console.log(concatenacion);
+
