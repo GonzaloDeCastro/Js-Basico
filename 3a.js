0d5dd47f0b0fd18da69491df466793c6a95ef79d
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-14 22:30:58 -0300

    3 - Arrays Ejercicio -a

---
+++ 3a.js
@@ -0,0 +1,13 @@
+/*
+Arrays
+a) Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5
+y 11 (utilizar console.log).
+*/
+
+var meses;
+
+meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+        "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log(meses[4]+ ' ' + meses[10]);        
