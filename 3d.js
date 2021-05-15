7c9278d312fd7124e16d3a8bc23c78632b65bf5c
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-15 03:07:54 -0300

    3 - Arrays Ejercicio - d

---
+++ 3d.js
@@ -0,0 +1,12 @@
+/*
+Arrays
+d) Quitar un elemento del principio y del final del array (utilizar shift y pop).
+*/
+
+var colores = ["Rojo","Azul","Verde","Amarillo","Violeta"];
+
+colores.shift();
+colores.pop();
+
+console.log(colores);
+
