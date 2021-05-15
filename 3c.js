9aae906bb0b0860a35fb988360e243f32672df73
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-15 03:03:24 -0300

    3 - Array Ejercicio - c

---
+++ 3c.js
@@ -0,0 +1,12 @@
+/*
+Arrays
+c) Agregar un elemento al principio y al final del array (utilizar unshift y push).
+*/
+
+
+var colores = ["Rojo","Azul","Verde","Amarillo","Violeta"];
+
+colores.push("Naranja");
+colores.unshift("Marron");
+
+console.log(colores);
