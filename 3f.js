820c51c2253441850bd03b67ddbab65ac499b473
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-15 03:32:12 -0300

    3 - Arrays Ejercicio - f

---
+++ 3f.js
@@ -0,0 +1,15 @@
+/*
+Arrays
+f) Unir todos los elementos del array en un único string donde cada mes este separado por un
+guión - (utilizar join).
+
+*/
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+        "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+meses = meses.join(" - ");
+
+
+console.log(meses);
+
