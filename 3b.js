aa5975a06929513a647a36bcccd2ce2c5d8c4641
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-15 00:21:58 -0300

    3 - Arrays Ejercicio - b

---
+++ 3b.js
@@ -0,0 +1,11 @@
+/*
+Arrays
+b) Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
+*/
+
+var meses;
+
+meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+        "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log(meses.sort());        
