14935311fe94f7855170fe520d7be0f7542a96b1
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-16 00:10:54 -0300

    5 - For Ejercicio - a

---
+++ 5a.js
@@ -0,0 +1,14 @@
+/*
+For
+a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de
+JavaScript para mostrar una alerta utilizando cada una de las palabras.
+*/
+
+var  Mascotas;
+
+Mascotas = ["Perro", "Gato", "Loro", "Tortuga", "Pez"];
+
+for ( i = 0; i < Mascotas.length; i++ ){
+
+    console.log(Mascotas[i]);
+}
