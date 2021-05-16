8b35355aa835048cedb52c24cc3a189f5d87fa23
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-16 04:22:24 -0300

    6 - Funciones Ejercicio - c

---
+++ 6c.js
@@ -0,0 +1,20 @@
+/*
+Funciones
+c) Crear una función validate integer que reciba un número como parámetro y devuelva verdadero
+si es un número entero.
+*/
+
+var resultado;
+
+function validaentero(a){ 
+    if ( a === parseInt(a)){
+        return resultado = true;
+    }
+    else{
+        return resultado = false;
+    }
+}
+
+validaentero(2);
+
+console.log(resultado);
