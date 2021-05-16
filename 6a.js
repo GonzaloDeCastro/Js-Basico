27710945d014febde49cbce7a268bb3826c4d560
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-16 03:28:45 -0300

    6 - Funciones Ejercicio - a

---
+++ 6a.js
@@ -0,0 +1,16 @@
+/*
+Funciones
+a) Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la
+función y guardar el resultado en una variable, mostrando el valor de dicha variable en la
+consola del navegador.
+*/
+
+var resultado;
+
+function suma(a,b){
+    return resultado = a + b;
+}
+
+suma(4,7)
+
+console.log(resultado);
